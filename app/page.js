adc17d44764d6ed36857170ffe7aac95ddf3e12c
unknown <coderanirudh@gmail.com>
2023-06-29 23:09:12 +0530

    auth pages created

--- app/page.js
+++ app/page.js
@@ -7,11 +7,11 @@ export default function Home() {
       <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
           <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            Possibly the best project showcase
+            Could this be the best project showcase?
           </h1>
           <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-            This project has everything you could ask for: Authentication, Chat
-            & Feed API and professional documentation
+            This project has everything from Authentication, Chat & Feed API and
+            professional documentation. Need more? I'm ready!
           </p>
           <a
             href="#"
@@ -32,7 +32,7 @@ export default function Home() {
             </svg>
           </a>
           <a
-            href="#"
+            href="https://github.com/kadianAnirudh/Stream"
             class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
           >
             Github
