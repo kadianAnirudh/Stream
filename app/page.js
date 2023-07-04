531bcae07bea8c7e94ab55f1595b8686b1b1d1ec
unknown <coderanirudh@gmail.com>
2023-07-04 12:56:47 +0530

    documentation link added

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
           </Link>
 
           <Link
-            href="https://github.com/kadianAnirudh/Stream"
+            href="https://maple-cheese-7fb.notion.site/Stream-Documentation-ccf487c908b74341a655cf4b3db2e85d?pvs=4"
             className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
           >
             Documentation
